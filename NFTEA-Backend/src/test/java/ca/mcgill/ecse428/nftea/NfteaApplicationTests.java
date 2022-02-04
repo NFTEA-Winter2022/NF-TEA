4b0c6fcff4b2192b6276821625a5d16f64d3e9a8
AlexandruB30 <73802110+AlexandruB30@users.noreply.github.com>
2022-02-04 16:01:00 -0500

    Basic setup of the project

---
+++ NFTEA-Backend/src/test/java/ca/mcgill/ecse428/nftea/NfteaApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.nftea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
