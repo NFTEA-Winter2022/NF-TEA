9471348942567de4610dd0d92cf85b8e3c0b0ef4
lunamor <menghongyi@gmail.com>
2022-02-11 17:13:43 -0500

    added error msgs on login page

--- NFTEA-Frontend/src/views/Login.vue
+++ NFTEA-Frontend/src/views/Login.vue
@@ -6,25 +6,27 @@
         <v-row>
           <v-text-field
             v-model="email"
-            :rules="emailRules"
             label="Email"
             required
           ></v-text-field>
         </v-row>
+        <span v-if="errorEmail" class="errorMessage">{{errorEmail}} </span>
         <v-row>
           <v-text-field
             v-model="password"
             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
-            :rules="passwordRules"
             :type="showPassword ? 'text' : 'password'"
             label="Password"
             required
             @click:append="showPassword = !showPassword"
           ></v-text-field>
         </v-row>
+        <span v-if="errorPassword" class="errorMessage">{{errorPassword}} </span>
+        <span v-if="errorStatus" class="errorMessage">{{errorStatus}} </span>
         <v-row>
           <v-col>
             <v-btn
+            @click="login(email, password)"
             > 
               Login
             </v-btn>
@@ -46,12 +48,32 @@
     margin: auto;
     width: 300px;
   }
+  .errorMessage {
+    color: red;
+  }
 </style>
 <script>
 export default {
   data() {
     return {
-      showPassword: false
+      showPassword: false,
+      errorEmail: '',
+      errorPassword: '',
+      errorStatus: ''
+    }
+  },
+  methods: {
+    login: function(email, password) {
+       if(email != "hello") {
+         this.errorEmail = 'Email is not associated with an account';
+         this.errorPassword = ''
+       } else if(password != "world") {
+         this.errorPassword = "Incorrect password";
+         this.errorEmail = '';
+       } else {
+         this.errorEmail = '';
+         this.errorPassword = '';
+       }
     }
   }
 }
