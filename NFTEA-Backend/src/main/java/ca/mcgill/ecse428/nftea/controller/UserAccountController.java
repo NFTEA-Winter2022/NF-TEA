1134e82f43990bffa4e207851443ae6dafb8bdae
Mircea Gosman <mirceagosman@gmail.com>
2022-02-12 22:50:24 -0500

    Removed testing code

--- NFTEA-Backend/src/main/java/ca/mcgill/ecse428/nftea/controller/UserAccountController.java
+++ NFTEA-Backend/src/main/java/ca/mcgill/ecse428/nftea/controller/UserAccountController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +17,6 @@ public class UserAccountController {
 
     @Autowired
     private UserAccountService userAccountService;
-    @GetMapping(value = {"/test", "/test/"})
-    public String Testtest (){
-    	return "the test has worked";
-    }
-
 
     @PostMapping(value = {"/user-account","/user-account/"})
     public ResponseEntity CreateCustomerAccount(
