c0de3f5f9c7248e8fb895604881843f650f11fb7
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-02-19 16:19:46 -0500

    edits on User Profiles Page

--- NFTEA-Frontend/src/components/UserProfilePage.vue
+++ NFTEA-Frontend/src/components/UserProfilePage.vue
@@ -1,31 +1,45 @@
 <template>
-    <div>
-        <h1>
-            User Profile
-        </h1>
-    </div>
+  <div class="UserProfile">
+    <h1>User Profile Page</h1>
+    <v-form v-model = "valid" id="userprofile-form">
+      <v-container>
+        <vue-metamask
+            userMessage="msg"
+            @onComplete="onComplete"
+        >
+        </vue-metamask>
+        <v-row>
+          <v-text-field
+            v-model="username"
+            label="Username"
+            required>
+          </v-text-field>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from '@vue/composition-api'
-import facebook from '../api/facebook.js'
+// import facebook from '../api/facebook.js'
 
 export default defineComponent({
 
     setup() {
-        insta = facebook.getInstagramContent();
-        id = insta.id;
-        media_type = insta.media_type;
-        username = insta.username;
-        media_type = insta.media_type;
+        // let insta = facebook.getInstagramContent();
+        // let userid = insta.id;
+        // let media_type = insta.media_type;
+        // let username = insta.username;
+        // let media_url = insta.media_url;
     },
 
    data: () => ({
-    insta: '',
-    id: '',
-    media_type:'',
-    username: '',
-    media_url: '',
+    // insta: this.insta,
+    //  userid: this.userid,
+    // media_type:this.media_type,
+    // username: '',
+    // media_url: '',
   }),
 })
 </script>
