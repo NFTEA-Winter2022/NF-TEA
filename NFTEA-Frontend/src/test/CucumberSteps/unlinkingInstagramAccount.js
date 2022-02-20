332895836b1c3b0f163cde61aed26c78ba2b7a08
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-02-20 17:50:23 -0500

    Update unlinkingInstagramAccount.js

--- NFTEA-Frontend/src/test/CucumberSteps/unlinkingInstagramAccount.js
+++ NFTEA-Frontend/src/test/CucumberSteps/unlinkingInstagramAccount.js
@@ -1,6 +1,6 @@
 const defineSupportCode = require('cucumber');
 const assert = require('assert');
-
+let message = "";
 
 
 defineSupportCode.Given("a NF-Tea's account {string} with a linked Instagram Account", async function (arg1) {
@@ -14,16 +14,29 @@ defineSupportCode.When("the user unlink their Instagram Account from their NF-Te
 });
 
 defineSupportCode.When("the Instagram Account is successfully unlinked from their account", async function () {
-
+    if (!document.cookie){
+        continue;
+    }
+    else{
+        error = "";
+    }
     return 'pending';
 });
 
 defineSupportCode.Then("a message shall be raised {string}", async function (arg1) {
-
-    return 'pending';
+    assert.equal(arg1,message);
 });
 
 defineSupportCode.Then("their Instagram account is not link to their NF-Tea's Account", async function () {
+    assert.equal(null, document.cookie);
+});
 
-    return 'pending';
+defineSupportCode.Then("their Instagram account is linked to their NF-Tea's Account", async function () {
+
+    assert.notEqual(null, document.cookie);
+
+});
+
+defineSupportCode.Then("an error shall be raised {string}", async function (arg1) {
+    assert.equal(arg1, message);
 });
