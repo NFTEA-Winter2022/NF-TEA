a9fddcc1caceb8be2c819f46f699d1ab8f8641c7
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-02-23 22:45:31 -0500

    Update deleteCollectionStepDefinitions.java

--- NFTEA-Backend/src/test/java/ca/mcgill/ecse428/nftea/CucumberStepDefinitions/deleteCollectionStepDefinitions.java
+++ NFTEA-Backend/src/test/java/ca/mcgill/ecse428/nftea/CucumberStepDefinitions/deleteCollectionStepDefinitions.java
@@ -4,14 +4,22 @@ import ca.mcgill.ecse428.nftea.dao.CollectionNFTRepository;
 import ca.mcgill.ecse428.nftea.model.CollectionNFT;
 import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
+
 public class deleteCollectionStepDefinitions {
 
     CollectionNFT collectionNFT = null;
+    String messageError = "";
+    int originalSize = 0;
 
     @Autowired
     CollectionNFTRepository collectionNFTRepository;
@@ -20,6 +28,7 @@ public class deleteCollectionStepDefinitions {
     @After
     public void teardown(){
         collectionNFTRepository.deleteAll();
+        originalSize = 0;
     }
 
     @Given("a collection of NFT:")
@@ -38,9 +47,38 @@ public class deleteCollectionStepDefinitions {
                 for (String w: temp) {
                     listOfNFT.add(Long.parseLong(w));
                 }
-                // TODO: Create a collec
+                originalSize = temp.length;
+                // TODO: Create a collection
 
             }
         }
     }
+
+    @When("a user deletes a collection {string}")
+    public void aUserDeletesACollection(String collectionID){
+        /*
+        * TODO: Use the service method
+        *
+        * */
+//        try{
+//
+//        }
+//        catch (){
+//
+//        }
+    }
+
+    @Then("the collection gets deleted")
+    public void theCollectionGetsDeleted(){
+
+//        assertTrue(originalSize, collectionNFTRepository.findByCollectionID());
+    }
+
+    @Then("a message shall be raised {string}")
+    public void aMessageShallBeRaised(String message){
+        assertTrue(messageError.contains(message));
+    }
+
+
+
 }
