ab4ea35f1896b85da50bbfd9570b9df6c67304a5
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2022-02-26 15:54:42 -0500

    Create NFT Button Successful

--- NFTEA-Frontend/src/components/UserProfilePage.vue
+++ NFTEA-Frontend/src/components/UserProfilePage.vue
@@ -43,13 +43,23 @@ export default ({
       }
     },
 
-    NFT(media) {
+    NFT(item) {
       try {
-        blockchain.mintNFT(media);
+        // let media = {
+        //   id: "17845582262609336",
+        //   publisherId: "1234567",
+        //   media_type: "IMAGE",
+        //   media_url: "https://scontent.cdninstagram.com/v/t51.29350-15/206753475_4211925482186688_8667004301827108956_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=xamL0zvoP3MAX-ocUXw&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8o-dgBxirB2GkzrEP1geiKf6Uea1Q2R5v08rLYdAmrBQ&oe=621F4BB9",
+        //   permalink: "https://www.instagram.com/p/CQjIASNgkVCPW1uejAnRiazoT40P43UsQYzUzQ0/",
+        //   thumbnail_url: "none",
+        //   timestamp: "2021-06-25T15:51:08+0000",
+        //   username: "Danurbae",
+        //   caption: "Summer vibes~",
+        // }
+        blockchain.mintNFT(item);
       } catch(e) {
-        console.log(e);
+        console.log(e)
       }
-
     }
   },
   beforeMount() {
